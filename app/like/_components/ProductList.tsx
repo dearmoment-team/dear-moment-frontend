917820a4afa3e6ea0cab53b5de6ea194186d5c23
dayoung <elinhue@gmail.com>
2025-03-20 22:22:43 +0900

    Feat: 좋아요 페이지 레이아웃 추가

--- app/like/_components/ProductList.tsx
+++ app/like/_components/ProductList.tsx
@@ -10,9 +10,6 @@ export default function ProductList() {
         <li>
           <ProductCard />
         </li>
-        <li>
-          <ProductCard />
-        </li>
       </ul>
     </div>
   );
