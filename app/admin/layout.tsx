5995b460f633b7fa01b34103d5137bf68438fb4f
JaeHyunGround <gothddlek@naver.com>
2025-03-23 16:41:26 +0900

    Refactor: admin root layout 수정

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AdminLayoutProps } from './type';
 
 const AdminLayout = ({ children }: AdminLayoutProps) => {
-  return <main className="w-screen h-screen flex flex-col justify-center items-center">{children}</main>;
+  return <main className="w-screen h-screen flex flex-col items-center">{children}</main>;
 };
 
 export default AdminLayout;
