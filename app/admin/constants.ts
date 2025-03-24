2c591ce126726ab798f1c3009086230918aef83d
JaeHyunGround <gothddlek@naver.com>
2025-03-24 15:53:27 +0900

    Feat : 옵션 값 상수화

---
+++ app/admin/constants.ts
@@ -0,0 +1,13 @@
+export const availablePeriods = ['2025년 상반기', '2025년 하반기', '2026년 상반기', '2026년 하반기'];
+export const cameraType = ['디지털', '필름'];
+export const correctionStyle = [
+  '모던한',
+  '시크한',
+  '차분한',
+  '빈티지한',
+  '동화같은',
+  '따듯한',
+  '몽환적인',
+  '화사한',
+  '내추럴한',
+];
