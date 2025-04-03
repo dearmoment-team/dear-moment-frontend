480e6a7e452d73cd1ccc1ea14aa5803e04ec590e
JaeHyunGround <gothddlek@naver.com>
2025-04-03 11:48:40 +0900

    Refactor : 버튼 타입 추가

--- app/admin/_components/LoginForm.tsx
+++ app/admin/_components/LoginForm.tsx
@@ -9,17 +9,19 @@ const LoginForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center gap-[1rem]">
       <input
-        {...register('loginId')}
+        {...register('loginId', { required: true })}
         placeholder="아이디"
         className="h-[4rem] w-[40.3rem] rounded-md border border-solid border-[#D8DDE3] px-[1.8rem] py-[0.9rem] focus:outline-none focus:ring-2 focus:ring-[#D8DDE3]"
       />
       <input
-        {...register('password')}
+        {...register('password', { required: true })}
         type="password"
         placeholder="비밀번호"
         className="h-[4rem] w-[40.3rem] rounded-md border border-solid border-[#D8DDE3] px-[1.8rem] py-[0.9rem] focus:outline-none focus:ring-2 focus:ring-[#D8DDE3]"
       />
-      <button className="mt-[0.9rem] h-[5.6rem] w-[40.3rem] rounded-md bg-[#3F3F3F] text-white">로그인</button>
+      <button type="submit" className="mt-[0.9rem] h-[5.6rem] w-[40.3rem] rounded-md bg-[#3F3F3F] text-white">
+        로그인
+      </button>
     </form>
   );
 };
