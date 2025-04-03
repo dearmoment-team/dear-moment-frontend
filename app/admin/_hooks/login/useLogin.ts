a0f55e4c4adfb211d573cf2af23bc077b3cbdae7
JaeHyunGround <gothddlek@naver.com>
2025-04-03 11:49:22 +0900

    Refactor : 스튜디오 보유 시 리다이렉트 url에 스튜디오 ID 값 추가

--- app/admin/_hooks/login/useLogin.ts
+++ app/admin/_hooks/login/useLogin.ts
@@ -7,7 +7,7 @@ import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
 export const useLogin = () => {
-  const { token, setToken } = adminTokenStore();
+  const { setToken } = adminTokenStore();
   const router = useRouter();
 
   const methods = useForm<LoginFormDataType>({
@@ -20,10 +20,13 @@ export const useLogin = () => {
   // TODO : 미들웨어 설정으로 토큰값 없을 시 studio, product 못 들어가도록 막기
   const onSubmit = async (data: LoginFormDataType) => {
     try {
-      await login(data).then(res => setToken(res.token));
-      await getProfile(token);
-      // TODO : studioId 값 유무에 따라 쿼리 스트링 추가
-      router.push('/admin/studio');
+      const loginResponse = await login(data);
+      setToken(loginResponse.token);
+
+      const { data: userData } = await getProfile(loginResponse.token);
+      const studioId = userData.data.studioId;
+      const url = studioId ? `/admin/studio?id=${studioId}` : '/admin/studio';
+      router.push(url);
     } catch (error) {
       console.error(error);
     }
