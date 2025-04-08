3a583243ad6ddc19f99f877975d3f04aba310713
JaeHyunGround <gothddlek@naver.com>
2025-04-08 16:44:22 +0900

    Refactor : 라우팅 설정 수정

--- app/admin/_components/NavBar.tsx
+++ app/admin/_components/NavBar.tsx
@@ -2,31 +2,47 @@
 
 import { usePathname, useRouter } from 'next/navigation';
 import clsx from 'clsx';
+import { studioIdStore } from '../_stores/studioIdStore';
+import { adminTokenStore } from '../_stores/adminTokenStore';
 
 const NavBar = () => {
+  const { token } = adminTokenStore();
+  const { studioId } = studioIdStore();
   const pathname = usePathname();
   const router = useRouter();
 
   return (
     <div className="mt-[10rem]">
-      <nav className="flex top-[0.9rem] w-[100vw] h-[5.3rem] text-[#0B0B0B] bg-[#E2DEDE]">
+      <nav className="top-[0.9rem] flex h-[5.3rem] w-[100vw] bg-[#E2DEDE] text-[#0B0B0B]">
         <section
-          onClick={() => router.push('/admin/studio')}
+          onClick={() => {
+            if (studioId) {
+              router.push(`/admin/studio?studioId=${studioId}`);
+            } else {
+              router.push('/admin/studio');
+            }
+          }}
           className={clsx(
-            'w-[15.1rem] font-semibold text-[1.4rem] flex justify-center items-center cursor-pointer',
+            'flex w-[15.1rem] cursor-pointer items-center justify-center text-[1.4rem] font-semibold',
             pathname.includes('studio') && 'bg-[#4D4242] text-[#FFFFFF]'
           )}
         >
           스튜디오 관리
         </section>
         <section
-          onClick={() => router.push('/admin/product')}
+          onClick={() => {
+            if (token && studioId) {
+              router.push(`/admin/product?studioId=${studioId}`);
+            } else {
+              alert('로그인 후, 상품 관리가 가능합니다.');
+            }
+          }}
           className={clsx(
-            'w-[15.1rem] font-semibold text-[1.4rem] flex justify-center items-center cursor-pointer',
+            'flex w-[15.1rem] cursor-pointer items-center justify-center text-[1.4rem] font-semibold',
             pathname.includes('product') && 'bg-[#4D4242] text-[#FFFFFF]'
           )}
         >
-          상품 관리 관리
+          상품 관리
         </section>
       </nav>
     </div>
