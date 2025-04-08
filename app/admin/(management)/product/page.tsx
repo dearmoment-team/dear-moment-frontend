18b63328e356ec96ad459d9b809bb28ea547894a
JaeHyunGround <gothddlek@naver.com>
2025-04-08 16:41:36 +0900

    Refactor : product 페이지 구조 수정

--- app/admin/(management)/product/page.tsx
+++ app/admin/(management)/product/page.tsx
@@ -1,21 +1,10 @@
 import ProductForm from '@/admin/_components/ProductForm';
-import ProductPackageOption from '@/admin/_components/ProductPackageOption';
-import ProductSingleOption from '@/admin/_components/ProductSingleOption';
 
 const ProductPage = () => {
   return (
-    <div className="flex flex-col gap-[6rem] max-w-[90rem] mx-auto my-[5rem]">
-      <section>
-        <header className="text-[2.4rem] font-semibold text-[#000000] mb-[1rem]">상품</header>
-        <ProductForm />
-      </section>
-      <section>
-        <header className="text-[2.4rem] font-semibold text-[#000000] mb-[1rem]">상품 옵션</header>
-        단품
-        <ProductSingleOption />
-        패키지
-        <ProductPackageOption />
-      </section>
+    <div className="mx-auto my-[5rem] flex max-w-[90rem] flex-col gap-[6rem]">
+      <header className="mb-[1rem] text-[2.4rem] font-semibold text-[#000000]">상품</header>
+      <ProductForm />
     </div>
   );
 };
