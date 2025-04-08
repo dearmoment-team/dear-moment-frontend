7d90a61cb9b1c9db590a1b99f67ef683471b705a
JaeHyunGround <gothddlek@naver.com>
2025-04-08 16:42:00 +0900

    Style : admin Header z-index 추가

--- app/admin/_components/Header.tsx
+++ app/admin/_components/Header.tsx
@@ -6,12 +6,7 @@ interface HeaderProps {
 }
 
 const Header = ({ className, children }: HeaderProps) => {
-  return (
-    <header className={clsx('cursor-default', className)}>
-      {/* TODO : 스튜디오 or 상품 수정 시 헤더 명을 ~~수정으로 바꿔줘야 함. 쿼리 스트링 사용해서 등록인지 수정인지 구분 필요. (정보 id 같은 걸 쿼리 스트링으로 넣으면 될듯) */}
-      {children}
-    </header>
-  );
+  return <header className={clsx('z-10 cursor-default', className)}>{children}</header>;
 };
 
 export default Header;
