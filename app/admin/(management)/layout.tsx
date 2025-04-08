e944b057f8f06a52f1ee24e65ea51204c346fc84
JaeHyunGround <gothddlek@naver.com>
2025-04-08 16:41:04 +0900

    Refactor : Header text 수정

--- app/admin/(management)/layout.tsx
+++ app/admin/(management)/layout.tsx
@@ -15,7 +15,7 @@ const FormPageLayout = ({ children }: AdminLayoutProps) => {
           'fixed left-0 top-0 flex w-full justify-center bg-[#2C2C2C] py-[1rem] text-[2.4rem] font-bold text-white'
         }
       >
-        {pathname.includes('studio') ? '스튜디오 등록' : '상품 등록'}
+        {pathname.includes('studio') ? '스튜디오' : '상품'}
       </Header>
       <NavBar />
       <section className="w-full px-[4rem]">{children}</section>
