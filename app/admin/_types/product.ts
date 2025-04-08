5bb768064b2e7ae09478b2ecca0802c556862417
JaeHyunGround <gothddlek@naver.com>
2025-04-08 16:45:44 +0900

    Feat : product 관련 타입 선언

---
+++ app/admin/_types/product.ts
@@ -0,0 +1,59 @@
+export interface ImageType {
+  imageId: number;
+  url: string;
+}
+
+interface PartnerShopType {
+  category: 'WEDDING_SHOP' | 'HAIR_MAKEUP' | 'DRESS' | 'MENS_SUIT' | 'BOUQUET' | 'VIDEO' | 'STUDIO' | 'ETC';
+  name: string;
+  link: string;
+}
+
+interface ProductOptionType {
+  optionId?: number;
+  productId?: number;
+  name: string;
+  optionType: 'SINGLE' | 'PACKAGE';
+  discountAvailable: boolean;
+  originalPrice: number;
+  discountPrice?: number;
+  description?: string;
+  costumeCount: number;
+  shootingLocationCount: number;
+  shootingHours: number;
+  shootingMinutes: number;
+  retouchedCount: number;
+  partnerShops?: PartnerShopType[];
+  originalProvided?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface ProductFormDataType {
+  productType: 'WEDDING_SNAP';
+  shootingPlace: 'JEJU';
+  title: string;
+  description: string;
+  availableSeasons: (
+    | 'YEAR_2025_FIRST_HALF'
+    | 'YEAR_2025_SECOND_HALF'
+    | 'YEAR_2026_FIRST_HALF'
+    | 'YEAR_2026_SECOND_HALF'
+  )[];
+  cameraTypes: ('DIGITAL' | 'FILM')[];
+  retouchStyles: ('MODERN' | 'CHIC' | 'CALM' | 'VINTAGE' | 'FAIRYTALE' | 'WARM' | 'DREAMY' | 'BRIGHT' | 'NATURAL')[];
+  detailedInfo: string;
+  contactInfo: string;
+  options: ProductOptionType[];
+
+  productId?: number;
+  createdAt?: string;
+  updatedAt?: string;
+
+  mainImage?: ImageType;
+  subImages?: ImageType[];
+  additionalImages?: ImageType[];
+  mainImageFile?: File;
+  subImageFiles?: File[];
+  additionalImageFiles?: File[];
+}
