4dc76a68eda90182a27fbe6862d844c3be7f38c7
WATA-Yiju <yiju.dev@watanow.com>
2025-04-16 14:15:29 +0900

    fix: Input 에러메세지 props 추가

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -4,10 +4,11 @@ import { forwardRef, useMemo, useState } from 'react';
 interface InputProps extends React.ComponentProps<'input'> {
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   value?: string;
+  errorMessage?: string; // 부모 컴포넌트에서 전달받는 에러 메시지
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = 'text', value = '', maxLength = 15, onChange, ...props }, ref) => {
+  ({ className, type = 'text', value = '', maxLength = 15, onChange, errorMessage, ...props }, ref) => {
     // type별 크기 설정
     const sizeClasses = {
       text: 'h-[5.2rem]',
@@ -45,11 +46,11 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             'bg-gray-10', // 배경색
             'px-[1.2rem] py-[1.6rem]', // padding 좌우 1.2rem, 위아래 1.6rem
             'text-label1Reading',
-            maxLengthOver && isFocused
-              ? 'border-red-50 ring-1 ring-red-50 focus:outline-none' // 15자 초과 → 빨간 테두리
+            (maxLengthOver && isFocused) || errorMessage
+              ? 'border-red-50 ring-1 ring-red-50 focus:outline-none' // 에러 상태 → 빨간 테두리
               : isFocused
               ? 'border-gray-20 focus:border-gray-20 focus:ring-1 focus:ring-gray-400 focus:outline-none' // 기본은 테두리 없음 → 포커스 시 회색 테두리
-              : 'border-transparent', // 포커스 없고 15자 이하 -> 테두리 없음
+              : 'border-transparent', // 포커스 없고 에러 없음 -> 테두리 없음
             sizeClasses[type as keyof typeof sizeClasses] || 'h-[5.2rem]',
             className
           )}
@@ -66,18 +67,18 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 
         {/* 경고문구 및 글자수 표시 */}
         <div className="flex justify-between h-[1.7rem] text-gray-50">
-          <span className={maxLengthOver && isFocused ? 'text-red-50' : ''}>
-            {maxLengthOver && isFocused ? warningMessage : ''}
+          <span className={((maxLengthOver && isFocused) || errorMessage) ? 'text-red-50' : ''}>
+            {errorMessage || (maxLengthOver && isFocused ? warningMessage : '')}
           </span>
           <span>
             <span
               className={
-                maxLengthOver && isFocused ? 'text-red-50' : value.length > 0 ? 'text-gray-90' : 'text-gray-50'
+                (maxLengthOver && isFocused) || errorMessage ? 'text-red-50' : value.length > 0 ? 'text-gray-90' : 'text-gray-50'
               }
             >
               {value.length}
             </span>
-            <span className={maxLengthOver ? 'text-gray-90' : 'text-gray-50'}> / {maxLength}</span>
+            <span className={maxLengthOver || errorMessage ? 'text-gray-90' : 'text-gray-50'}> / {maxLength}</span>
           </span>
         </div>
       </div>
