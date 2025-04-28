d2d4dec9450583d7f603485445389a9d31fbe6e5
JaeHyunGround <gothddlek@naver.com>
2025-04-28 23:43:29 +0900

    Fix : build error fix

--- app/admin/(management)/layout.tsx
+++ app/admin/(management)/layout.tsx
@@ -4,22 +4,25 @@ import NavBar from '../_components/NavBar';
 import Header from '../_components/Header';
 import { usePathname } from 'next/navigation';
 import { AdminLayoutProps } from '../_types/common';
+import { Suspense } from 'react';
 
 const FormPageLayout = ({ children }: AdminLayoutProps) => {
   const pathname = usePathname();
 
   return (
-    <>
-      <Header
-        className={
-          'fixed left-0 top-0 flex w-full justify-center bg-[#2C2C2C] py-[1rem] text-[2.4rem] font-bold text-white'
-        }
-      >
-        {pathname.includes('studio') ? '스튜디오' : '상품'}
-      </Header>
-      <NavBar />
-      <section className="w-full px-[4rem]">{children}</section>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <>
+        <Header
+          className={
+            'fixed left-0 top-0 flex w-full justify-center bg-[#2C2C2C] py-[1rem] text-[2.4rem] font-bold text-white'
+          }
+        >
+          {pathname.includes('studio') ? '스튜디오' : '상품'}
+        </Header>
+        <NavBar />
+        <section className="w-full px-[4rem]">{children}</section>
+      </>
+    </Suspense>
   );
 };
 
