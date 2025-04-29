959512e4785c19d7b8a7bc8d01261091541fba30
WATA-Yiju <yiju.dev@watanow.com>
2025-04-29 09:56:42 +0900

    fix: Suspense fallback LoadingSpinner 수정

--- app/login/callback/success/page.tsx
+++ app/login/callback/success/page.tsx
@@ -1,12 +1,13 @@
-'use client';
+"use client";
 
-import { Suspense } from 'react';
-import LoginSuccessPageUI from './page-ui';
+import LoadingSpinner from "@/components/LoadingSpinner";
+import { Suspense } from "react";
+import LoginSuccessPageUI from "./page-ui";
 
 export default function LoginSuccessPage() {
-  return (
-    <Suspense fallback={<div>로딩 중...</div>}>
-      <LoginSuccessPageUI />
-    </Suspense>
-  );
+	return (
+		<Suspense fallback={<LoadingSpinner />}>
+			<LoginSuccessPageUI />
+		</Suspense>
+	);
 }
